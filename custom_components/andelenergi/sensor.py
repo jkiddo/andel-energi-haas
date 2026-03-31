58ff0a5e3bb095eb64709dbfcb0b676575145f84
Jens Kristian Villadsen <jenskristianvilladsen@gmail.com>
2026-03-31 13:11:54 +0200

    revert

--- custom_components/andelenergi/sensor.py
+++ custom_components/andelenergi/sensor.py
@@ -96,9 +96,7 @@ class AndelEnergiCurrentPrice(SensorEntity):
     Returns price with and without transport & taxes.
     """
 
-    _attr_device_class = SensorDeviceClass.MONETARY
-    _attr_state_class = SensorStateClass.MEASUREMENT
-    _attr_native_unit_of_measurement = "DKK"
+    _attr_native_unit_of_measurement = "DKK/kWh"
     _attr_icon = "mdi:currency-usd"
     _attr_suggested_display_precision = 2
 
